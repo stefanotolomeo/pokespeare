162cac0aeba124eeda4f4e609bd8413f977305cf
Stefano Tolomeo <ste.tolomeo@gmail.com>
2020-09-13 17:24:29 +0200

    Manual IT for HttpClient

--- src/it/java/com/company/pokespeare/http/client/HttpClientIT.java
+++ src/it/java/com/company/pokespeare/http/client/HttpClientIT.java
@@ -1,4 +1,31 @@
 package com.company.pokespeare.http.client;
 
-public class HttpClientIT {
+import com.company.pokespeare.config.BaseIT;
+import com.company.pokespeare.http.model.BaseHttpRequest;
+import com.company.pokespeare.http.model.BaseHttpResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+// Manual test
+// @Disabled
+class HttpClientIT extends BaseIT {
+
+	@Inject
+	private HttpClient httpClient;
+
+	private final String pokemonUri = "https://pokeapi.co/api/v2/pokemon/";
+
+	@Test
+	void makeGetRequest_Test() throws Exception {
+		String pokemonDitto = "ditto";
+		String completeUri = pokemonUri + pokemonDitto;
+		BaseHttpRequest httpRequest = new BaseHttpRequest(completeUri);
+		BaseHttpResponse response = httpClient.makeGetRequest(httpRequest);
+
+		Assertions.assertNotNull(response);
+	}
 }
